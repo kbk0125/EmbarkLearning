8bddb6c7e0891ce65042519f9aa058a985c4c152
Kevin Kononenko <kevin.kononenko@gmail.com>
2015-10-08 01:08:31 -0400

    Another push?

---
+++ gruntfile.js
@@ -0,0 +1,81 @@
+// Gruntfile.js
+
+// our wrapper function (required by grunt and its plugins)
+// all configuration goes inside this function
+module.exports = function(grunt) {
+
+  // ===========================================================================
+  // CONFIGURE GRUNT ===========================================================
+  // ===========================================================================
+  
+  grunt.initConfig({
+
+    // get the configuration info from package.json ----------------------------
+    // this way we can use things like name and version (pkg.name)
+    
+    pkg: grunt.file.readJSON('package.json'),
+
+    // all of our configuration will go here
+    // configure jshint to validate js files -----------------------------------
+    jshint: {
+      options: {
+        reporter: require('jshint-stylish') // use jshint-stylish to make our errors look and read good
+      },
+
+      // when this task is run, lint the Gruntfile and all js files in src
+      build: ['gruntfile.js', 'public/js/**/*.js']
+    },
+    stylus: {
+      build: {
+        options: {
+          linenos: true,
+          compress: false
+        },
+        files: [{
+          expand: true,
+          cwd: 'src/styles',
+          src: [ '**/*.styl' ],
+          dest: 'public/css',
+          ext: '.css'
+        }]
+      }
+    },
+    nodemon: {
+      dev: {
+        script: 'server.js'
+      }
+    },
+    watch: {
+      // for stylesheets, watch css and less files 
+      // only run stylus stylesheets: 
+      css: {
+        files: ['src/styles/*.styl'], 
+        tasks: ['stylus'] 
+      },
+      // for scripts, run jshint and uglify 
+      scripts: { 
+        files: 'public/js/*.js', 
+        tasks: ['jshint'] 
+      } 
+    },
+    concurrent: {
+      target: {
+          tasks: ['nodemon', 'watch'],
+          options: {
+              logConcurrentOutput: true
+          }
+      }
+    }
+  });
+  grunt.registerTask('default', ['concurrent']); 
+  // ===========================================================================
+  // LOAD GRUNT PLUGINS ========================================================
+  // ===========================================================================
+  // we can only load these if they are in our package.json
+  // make sure you have run npm install so our app can find these
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-concurrent');
+};
---
+++ public/js/index.js
@@ -0,0 +1,195 @@
+// show/hide lists on sidebar
+$(".catHead").click(function(){
+	$(this).children('i').toggleClass('fa-chevron right fa-chevron-down');
+	$(this).siblings('ul').toggle('blind');
+});
+
+//Show dif guides in main window
+$('.listSelect').click(function(){
+	var keyData = $(this).data('filter');
+	$('.listSelect').removeClass('listHighlight');
+	$(this).addClass('listHighlight');
+	$('#opening').hide();
+	$('.learningCat').show();
+	$('.userPath').remove();
+	$('.learningCat').find('form').children('.basicInfo').children('.nameOfCat').val(keyData);
+	//AJAX to get list of user entered forms, but only if there are already entries in that category
+	$.ajax({
+		type:'GET',
+		url: '/planList',
+		headers: { 'Access-Control-Allow-Origin': '*' },
+		crossDomain: true,
+		data: {listKey: keyData}
+	})
+	.done(function(planList){
+		//Add user submitted plans to the list based on what users have added in past
+		if(planList.length)
+			addPlan(planList);
+	});
+});
+
+//Bring Up the form
+$('.intro').click(function(){
+	$(this).hide();
+	$(this).siblings('.form').fadeIn();
+});
+
+
+//Filling out your key resources
+$('.formBtn').click(function(){
+	var emptyRad;
+	var emptyTxt = $(this).siblings('input:text, textarea').filter(function() {
+		return this.value === "";
+	});
+	if($(this).siblings('.radioSet').length)
+		emptyRad= $(this).siblings('.radioSet').children('input:radio').filter(':checked').length;
+	else
+		emptyRad = 1;
+
+	if(!emptyTxt.length && emptyRad >0){
+		$(this).parent().hide();
+		$(this).parent().next().fadeIn();
+		//create radio buttons
+		$(this).parent().next().children( ".radioSet" ).buttonset();
+		$('#formError').hide();
+	}
+	else{
+		$('#formError').fadeIn().text('Please fill out each field');
+	}
+});
+
+//set 7 word limit to some fields
+$('.wordLimit').keypress(function(e){
+	var words = $(this).val().split(' ');
+	if(words.length >7)
+		e.preventDefault();
+});
+
+//Counter as user types
+$('.descLimit').keypress(function(){
+	$(this).prev().text('Brief Description ('+$(this).val().length+'/100):');
+});
+
+//Validate URLs for user input
+function validateURL(textval) {
+  var urlregex = new RegExp(
+        "^(http|https|ftp)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+))*$");
+  return urlregex.test(textval);
+}
+$('.validUrl').blur(function(){
+	var url= $(this).val();
+	var isValid=validateURL(url);
+	if(isValid)
+		$('#formError').fadeIn().text('This is a good url');
+	else
+		$('#formError').fadeIn().text('Please submit valid url');
+});
+
+// Add the plan once the user adds it
+function addPlan(planData){
+	// Array looks like : Plan Title, First Name of Author, Brief Advice, Label 1, Resource 1 Name, Resource 1 Link, Resource 1 Desc, etc.
+	var topSegment = '<div class="titleTop"><h3>'+planData[0]+'</h3><p> By: '+planData[1]+'</div><p class="advice">' +planData[2]+ '</p>';
+	var allBod= [];
+
+	//If it is new, put it in new category. If part of repeated category, put it in bucket with similar ones.
+	for(var i =3; i < planData.length-2; i+=4){
+		var resource, index, link, desc, catTitle;
+		if(allBod.indexOf('<h3>'+planData[i]+'</h3>') > -1){
+			index = allBod.indexOf('<h3>'+planData[i]+'</h3>');
+			desc = '<p>'+planData[i+3]+'</p>';
+			allBod.splice(index+1, 0, desc);
+			resource='<p>'+planData[i+1]+'</p>';
+			link = '<a href="http://'+planData[i+2]+'" target="blank">'+resource+'</a>';
+			allBod.splice(index+1, 0, link);
+		}
+		else{
+			catTitle = '<h3>'+planData[i]+'</h3>';
+			allBod.push(catTitle);
+			resource='<p>'+planData[i+1]+'</p>';
+			link = '<a href="http://'+planData[i+2]+'" target="blank">'+resource+'</a>';
+			allBod.push(link);
+			desc = '<p>'+planData[i+3]+'</p>';
+			allBod.push(desc);
+		}
+	}
+	var bottomBar='<div class="popBar"><p class="upTotal"></p><i class="fa fa-thumbs-o-up upvoteBtn"></i></div>';
+		
+	//Create the path with a header, the suggested items and an upvote option
+	var pathContain = '<div class="path userPath" data-uniqueid="0">'+topSegment+'<div class="list">'+allBod.join('')+ '</div>' +bottomBar+'</div>';
+
+	$('.pathsBody').append(pathContain);
+	//Count number of upvotes done, but how to localize this in scope without long jquery selector?
+	$('.pathsBody').children('.userPath').last().find('.upTotal').text(planData[planData.length-2]);
+	// make sure unique ID is available on front end
+	$('.pathsBody').children('.userPath').last().data('uniqueid', planData[planData.length-1]);
+}
+
+//Find which submit button was clicked within form
+$("form input[type=submit]").click(function() {
+    $("input[type=submit]", $(this).parents("form")).removeAttr("clicked");
+    $(this).attr("clicked", "true");
+});
+
+$('form').on('submit', function(ev){
+	ev.preventDefault();
+	var submitBtn = $("input[type=submit][clicked=true]");
+	//make sure all appropriate fields are filled
+	var emptyRad;
+	var emptyTxt = $(submitBtn).siblings('input:text, textarea').filter(function() {
+		return this.value === "";
+	});
+	if($(submitBtn).siblings('.radioSet').length)
+		emptyRad= $(submitBtn).siblings('.radioSet').children('input:radio').filter(':checked').length;
+	else
+		emptyRad = 1;
+
+	if(!emptyTxt.length && emptyRad >0){
+		//go forward with AJAX call
+		var form = $(this);
+		//eliminate empty fields
+		form.find(':input').filter(function() { return !this.value; }).attr('disabled', 'disabled');
+		var newPlan = form.serialize();
+		$.ajax({
+			type:'POST',
+			url: '/addPlan',
+			headers: { 'Access-Control-Allow-Origin': '*' },
+			crossDomain: true,
+			data: newPlan
+		})
+		.done(function(planData){
+			// add plan to list
+			addPlan(planData);
+
+			//reset form
+			form.trigger('reset');
+			var sameCat = $('.listHighlight').data('filter');
+			form.children('.basicInfo').children('.nameOfCat').val(sameCat);
+			form.find(':input').removeAttr('disabled');
+			$('.chunk').hide();
+			$('.basicInfo').show();
+			form.parent().hide();
+			$('.intro').fadeIn();
+		});
+	}
+	else{
+		$('#formError').fadeIn().text('Please fill out each field before submission');
+	}
+});
+
+//User can add only one vote to indicate their support for a plan, currently can refresh the page and then upvote again which needs to be fixed
+$('.pathsBody').on('click', ".upvoteBtn", function(){
+	var upvoteCount=parseInt($(this).siblings('.upTotal').text());
+	upvoteCount++;
+	$(this).css('color', '#3498db');
+	$(this).siblings('.upTotal').text(upvoteCount);
+	$(this).click(false);
+	var newVoteCount=parseInt($(this).siblings('.upTotal').text());
+	var uniqueID = $(this).parents('.userPath').data('uniqueid');
+	$.ajax({
+		type:'POST',
+		url: '/addVote',
+		headers: { 'Access-Control-Allow-Origin': '*' },
+		crossDomain: true,
+		data: {myid:uniqueID, votecount:newVoteCount}
+	});
+});
