283e52d37fe278a74295724d9d9fe9b49a836170
Kevin Kononenko <kevin.kononenko@gmail.com>
2016-03-04 13:42:29 -0500

    No connection.end

--- server.js
+++ server.js
@@ -169,10 +169,6 @@ app.post('/addVote', function(req, res){
 
 app.use('/', basicRouter)
 
-connection.end(function(err) {
-  if (err) throw err;
-});
-
 app.listen(process.env.PORT || 8080, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
