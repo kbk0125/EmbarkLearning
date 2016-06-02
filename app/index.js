6fd8b32d8ea1bec317716590fb6743c5db231747
Kevin Kononenko <kevin.kononenko@gmail.com>
2016-06-02 16:55:33 -0400

    dev-server

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ var routes = require('./config/routes')
 
 //FULL GUIDE http://www.christianalfoni.com/articles/2015_04_19_The-ultimate-webpack-setup
 //https://github.com/christianalfoni/webpack-express-boilerplate
+//http://www.christianalfoni.com/articles/2015_04_19_The-ultimate-webpack-setup
 
 ReactDOM.render(
 	routes,
