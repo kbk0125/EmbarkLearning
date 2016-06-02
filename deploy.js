044b036b56267274d150fb9e094d19b80a654934
Kevin Kononenko <kevin.kononenko@gmail.com>
2016-06-02 17:03:02 -0400

    another idea

--- deploy.js
+++ deploy.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === 'production') {
   // We basically just create a child process that will run
   // the production bundle command
   var child_process = require('child_process');
-  child_process.exec("webpack -p --config webpack.production.config.js", function (error, stdout, stderr) {
+  child_process.exec("rimraf dist && cross-env NODE_ENV=production webpack --config ./webpack.production.config.js --progress --profile --colors", function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
     if (error !== null) {
