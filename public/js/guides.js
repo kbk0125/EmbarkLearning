5b1d8ba6cb43dfa784fe5ed3f54af430b76bdca2
Kevin Kononenko <kevin.kononenko@gmail.com>
2017-11-28 23:14:09 -0500

    Code Analogies launch

--- public/js/guides.js
+++
@@ -1,8 +0,0 @@
-$('.segment').click(function(){
-	$('.segment').removeClass('chosen')
-	$(this).addClass('chosen')
-	var data = $(this).data('topic')
-	$('.pane').hide(function(){
-		$('div[data-pane="'+data+'"]').show()
-	})
-})
